92b20f2f34336f172192f1ec0c8d4bec66e617ed
sharma-akshat <72413891+sharma-akshat@users.noreply.github.com>
2022-08-24 14:42:30 +0530

    Amount of water trapped between pillars

--- Histogram.java
+++ Histogram.java
@@ -3,11 +3,15 @@ package ProgrammingPathshala;
 public class Histogram {
     public static void main(String gg[])
     {
-        int array[]={0,1,0,2,1,3,0,2};
-        int n=array.length;
-        int pm[]=new int[n];
-        int sm[]=new int[n];
-        int amt=0;
+        Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		long array[]=new long[n];
+		for(int i=0;i<n;i++){
+			array[i]=sc.nextLong();
+		}
+		long pm[]=new long[n];
+        long sm[]=new long[n];
+        long amt=0;
         pm[0]=array[0];
         for(int i=1;i<n;i++){
             pm[i]=Math.max(pm[i-1],array[i]);
@@ -17,14 +21,15 @@ public class Histogram {
             sm[j]=Math.max(sm[j+1],array[j]);
         }
         for(int i=1;i<=n-2;i++){
-            int h=array[i];
-            int h1=pm[i-1];
-            int h2=sm[i+1];
-            int dech=Math.min(h1,h2);
+            long h=array[i];
+            long h1=pm[i-1];
+            long h2=sm[i+1];
+            long dech=Math.min(h1,h2);
             if(dech<=h) continue;
             else amt+=(dech-h);
         }
-        System.out.println("Result is: "+amt);
+        System.out.println(amt);
+        sc.close();
     }
 }
 
